af40a32713fedbc2c0d1e8df3d2e654a9458c580
D3v <deviljin112@users.noreply.github.com>
2020-11-05 15:37:52 +0000

    Readme + Pseudo

--- scrabble.py
+++ scrabble.py
@@ -1,5 +1,8 @@
+# Creates a Scrabble class
 class Scrabble:
+    # Initialises the class with a input of the word used for scoring
     def __init__(self, word):
+        # Creates tables for each score
         self.value_1 = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"]
         self.value_2 = ["D", "G"]
         self.value_3 = ["B", "C", "M", "P"]
@@ -7,14 +10,21 @@ class Scrabble:
         self.value_5 = ["K"]
         self.value_8 = ["J", "X"]
         self.value_10 = ["Q", "Z"]
+        # Assigns the input to a class variable
         self.word = word
 
+    # Function for counting the score
     def count_score(self):
+        # Breaks the word into single letters
         letters = [k for k in self.word]
 
+        # Counts the score
         score = 0
+        # Loops through each letter
         for i in letters:
+            # Checks if the letter is in any class variable list
             if i in self.value_1:
+                # If so add respective points
                 score += 1
             elif i in self.value_2:
                 score += 2
@@ -29,13 +39,17 @@ class Scrabble:
             elif i in self.value_10:
                 score += 10
 
+        # Returns the final score
         return score
 
 
 def main():
+    # Input for the word used to calculate the score
     word = input("Please provide a word for scoring\n=> ")
 
+    # Intialisation of the class
     game = Scrabble(word.upper())
+    # Final print of the score
     print(f"Word Scored: {game.count_score()} points")
 
 
